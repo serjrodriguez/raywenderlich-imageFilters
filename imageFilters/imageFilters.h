272492f6782b938c9349f6b5ce4a256471ea9d94
Sergio Rodriguez <sergio.rodriguez@gonet.us>
2017-05-17 14:24:41 -0500

    Initial Commit

---
+++ imageFilters/imageFilters.h
@@ -0,0 +1,13 @@
+//
+//  imageFilters.h
+//  imageFilters
+//
+//  Created by Sergio Rodriguez on 17/05/17.
+//  Copyright © 2017 Sergio Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface imageFilters : NSObject
+
+@end
